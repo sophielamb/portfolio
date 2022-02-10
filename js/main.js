249c68feca05004d0345f711ca51ae1663b186b4
Sophie Lamb <69150936+sophielamb@users.noreply.github.com>
2022-02-10 21:01:27 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+// Menu collapse
+function collapseFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// Sticky nav
+window.onscroll = function() {stickyFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// JQuery Get year for copyright
+$('#year').text(new Date().getFullYear());
+
+});
